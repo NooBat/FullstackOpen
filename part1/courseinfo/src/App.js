dc547efd8b79762581678e213df8e2e33ad6a6a7
NooBat <danielnguyen2701@gmail.com>
2022-02-12 17:49:43 +0700

    commit by vscode

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,24 +1,70 @@
 import React from 'react';
 
-const Hello = (props) => {
+const Part = (props) => {
   return (
-    <div>
+    <>
       <p>
-        Hello {props.name}, you are {props.age} years old.
+        {props.name} {props.num}
       </p>
-    </div>
+    </>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.name}</h1>
+    </>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+  return (
+    <>
+      <Part name={props.parts[0].name} num={props.parts[0].exercises}/>
+      <Part name={props.parts[1].name} num={props.parts[1].exercises}/>
+      <Part name={props.parts[2].name} num={props.parts[2].exercises}/>
+    </>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <>
+      <p>
+        Number of exercise {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+      </p>
+    </>
   )
 }
 
 const App = () => {
-  const name = 'Peter'
-  const age = 15
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="George" age="20" />
-      <Hello name={name} age={age} />
+      <Header name={course.name} />
+      <Content 
+        parts={course.parts}  
+      />
+      <Total parts={course.parts} />
     </div>
   )
 }
