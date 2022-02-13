37f6e810f22842989d85797b0e2a0857616b6ad9
Noobat <khoi27012003@gmail.com>
2022-02-14 00:30:11 +0700

    push

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
 
 const Part = (props) => {
   return (
@@ -7,66 +8,67 @@ const Part = (props) => {
         {props.name} {props.num}
       </p>
     </>
-  )
-}
+  );
+};
 
 const Header = (props) => {
   return (
     <>
       <h1>{props.name}</h1>
     </>
-  )
-}
+  );
+};
 
 const Content = (props) => {
-  console.log(props)
+  console.log(props);
   return (
     <>
-      <Part name={props.parts[0].name} num={props.parts[0].exercises}/>
-      <Part name={props.parts[1].name} num={props.parts[1].exercises}/>
-      <Part name={props.parts[2].name} num={props.parts[2].exercises}/>
+      <Part name={props.parts[0].name} num={props.parts[0].exercises} />
+      <Part name={props.parts[1].name} num={props.parts[1].exercises} />
+      <Part name={props.parts[2].name} num={props.parts[2].exercises} />
     </>
-  )
-}
+  );
+};
 
 const Total = (props) => {
   return (
     <>
       <p>
-        Number of exercise {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+        Number of exercise{" "}
+        {props.parts[0].exercises +
+          props.parts[1].exercises +
+          props.parts[2].exercises}
       </p>
     </>
-  )
-}
+  );
+};
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  };
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  };
+
+  console.log(allClicks)
 
   return (
     <div>
-      <Header name={course.name} />
-      <Content 
-        parts={course.parts}  
-      />
-      <Total parts={course.parts} />
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
     </div>
-  )
-}
+  );
+};
 
 export default App;
