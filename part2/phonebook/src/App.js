aafe2fd15aa3517ba335f63b9ffe376863b81b33
NooBat <danielnguyen2701@gmail.com>
2022-02-15 01:26:45 +0700

    commit by vscode

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useState } from 'react';
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    {name: 'Arto Hellas'}
+  ])
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default App;
