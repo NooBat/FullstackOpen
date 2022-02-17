7fb72a36edfa773c0ebd202c2297f3dec4c96da9
Noobat <khoi27012003@gmail.com>
2022-02-18 00:27:41 +0700

    notes

---
+++ notes/src/services/notes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3001/notes'
+
+const getAll = () => {
+  return axios.get(baseUrl)
+}
+
+const create = newObject => {
+  return axios.post(baseUrl, newObject)
+}
+
+const update = (id, newObject) => {
+  return axios.put(`baseUrl`)
+}
