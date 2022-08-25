f83b4980865ae7381aa3a1987f1840fd298838f1
NooBat <khoi27012003@gmail.com>
2022-08-25 16:57:18 +0700

    finish exercises 7.7 country-hook

---
+++ part7/country-hook/src/hooks/index.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export const useField = (type) => {
+  const [value, setValue] = useState('');
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return {
+    type,
+    value,
+    onChange,
+  };
+};
+
+export const useCountry = (name) => {
+  const [country, setCountry] = useState(null);
+
+  useEffect(() => {
+    if (name) {
+      axios
+        .get(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
+        .then((response) => response.data)
+        .then((data) => setCountry(data[0]))
+        .catch((e) => e);
+    }
+  }, [name]);
+
+  return country;
+};
