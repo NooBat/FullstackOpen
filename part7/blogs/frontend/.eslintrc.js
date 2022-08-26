adc7cebf5d4de8f727669a89db15b71c292ebd91
NooBat <khoi27012003@gmail.com>
2022-08-27 00:47:17 +0700

    commit by vscode

---
+++ part7/blogs/frontend/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    "jest/globals": true,
+  },
+  extends: ["plugin:react/recommended", "airbnb"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "jest"],
+  rules: {
+    quotes: ["error", "single", { avoidEscape: true }],
+    "jsx-quotes": ["error", "prefer-single"],
+    "react/prop-types": 0,
+    "react/function-component-definition": [
+      2,
+      { namedComponents: "arrow-function" },
+    ],
+    "import/no-extraneous-dependencies": [
+      "error",
+      { devDependencies: ["**/*.test.js", "**/*.spec.js", "**/*.config.js"] },
+    ],
+  },
+};
