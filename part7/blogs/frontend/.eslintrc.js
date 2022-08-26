f07e2e445ef08e1f938ccc817c71d1522cb8fc78
NooBat <khoi27012003@gmail.com>
2022-08-27 00:57:31 +0700

    commit by vscode

--- part7/blogs/frontend/.eslintrc.js
+++ part7/blogs/frontend/.eslintrc.js
@@ -2,26 +2,30 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    "jest/globals": true,
+    'jest/globals': true,
   },
-  extends: ["plugin:react/recommended", "airbnb"],
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+  ],
   overrides: [],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["react", "jest"],
+  plugins: ['react', 'jest'],
   rules: {
-    quotes: ["error", "single", { avoidEscape: true }],
-    "jsx-quotes": ["error", "prefer-single"],
-    "react/prop-types": 0,
-    "react/function-component-definition": [
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'jsx-quotes': ['error', 'prefer-single'],
+    'react/prop-types': 0,
+    'react/function-component-definition': [
       2,
-      { namedComponents: "arrow-function" },
+      { namedComponents: 'arrow-function' },
     ],
-    "import/no-extraneous-dependencies": [
-      "error",
-      { devDependencies: ["**/*.test.js", "**/*.spec.js", "**/*.config.js"] },
+    'import/no-extraneous-dependencies': [
+      'error',
+      { devDependencies: ['**/*.test.js', '**/*.spec.js', '**/*.config.js'] },
     ],
   },
 };
