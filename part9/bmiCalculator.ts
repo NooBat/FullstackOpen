bd6cc2134b4f0e47ae2fb3ae6384abfe31ec20d1
NooBat <khoi27012003@gmail.com>
2022-09-06 19:29:32 +0700

    commit by vscode

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -1,28 +1,53 @@
-const bmiCalculator = (height: number, mass: number) : string => {
+const bmiCalculator = (height: number, mass: number): string => {
   const bmi: number = mass / Math.pow(height / 100, 2);
-  
+
   if (bmi < 16) {
-    return "Underweight (Severe thinness)";
+    return 'Underweight (Severe thinness)';
   } else if (bmi < 17) {
-    return "Underweight (Moderate thinness)";
+    return 'Underweight (Moderate thinness)';
   } else if (bmi < 18.5) {
-    return "Underweigth (Mide thinness)";
+    return 'Underweigth (Mide thinness)';
   } else if (bmi < 25) {
-    return "Normal (Heathy weight)";
+    return 'Normal (Heathy weight)';
   } else if (bmi < 30) {
-    return "Overweight (Pre-obese)";
+    return 'Overweight (Pre-obese)';
   } else if (bmi < 35) {
-    return "Obese (Class I)";
+    return 'Obese (Class I)';
   } else if (bmi < 40) {
-    return "Obese (Class II)";
+    return 'Obese (Class II)';
   }
 
-  return "Obese (Class III)"
+  return 'Obese (Class III)';
 };
 
 interface BmiValues {
-  height: number,
-  mass: number
+  height: number;
+  mass: number;
 }
 
-console.log(bmiCalculator(175, 80))
+const parseArguments = (args: Array<string>): BmiValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      mass: Number(args[3]),
+    };
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+};
+
+try {
+  const { height, mass } = parseArguments(process.argv);
+  bmiCalculator(height, mass);
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
+
+export default bmiCalculator;
