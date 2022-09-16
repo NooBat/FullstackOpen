db77f37ecf4e9a2f087f492066ebf6e3b75d5423
NooBat <khoi27012003@gmail.com>
2022-09-16 22:50:46 +0700

    commit nextjs-blog

--- nextjs-blog/pages/index.jsx
+++ nextjs-blog/pages/index.jsx
@@ -1,9 +1,21 @@
 import Head from 'next/head';
+import Link from 'next/link';
 
+import Date from '../components/Date';
 import Layout, { siteTitle } from '../components/Layout';
+import { getSortedPostsData } from '../lib/posts';
 import utilStyles from '../styles/utils.module.scss';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -19,6 +31,23 @@ export default function Home() {
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={(utilStyles.headingMd, utilStyles.padding1px)}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li key={id}>
+              <Link href={`/posts/${id}`} passHref>
+                <a href='replace'>{title}</a>
+              </Link>
+              <p>
+                <small className={utilStyles.lightText}>
+                  <Date dateString={date} />
+                </small>
+              </p>
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
---
+++ nextjs-blog/pages/posts/[id].jsx
@@ -0,0 +1,41 @@
+import parse from 'html-react-parser';
+import Head from 'next/head';
+
+import Date from '../../components/Date';
+import Layout from '../../components/Layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import utilStyles from '../../styles/utils.module.scss';
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        {parse(postData.contentHtml)}
+      </article>
+    </Layout>
+  );
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
