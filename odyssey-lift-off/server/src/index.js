9930a3a32d138e6060ee751765fd310a29703752
NooBat <khoi27012003@gmail.com>
2022-09-18 02:50:03 +0700

    commit by vscode

--- odyssey-lift-off/server/src/index.js
+++ odyssey-lift-off/server/src/index.js
@@ -1,7 +1,7 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 
-const mocks = {};
+// const mocks = {};
 
 const server = new ApolloServer({ typeDefs });
 
