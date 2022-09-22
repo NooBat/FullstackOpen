9702bea3e869a5b6ce19f1627cdf82d2cde8b473
NooBat <khoi27012003@gmail.com>
2022-09-22 21:58:24 +0700

    added odyssey-airlock

---
+++ odyssey-airlock/server/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  overrides: [
+    {
+      files: ['*.js'],
+      processor: '@graphql-eslint/graphql',
+    },
+    {
+      files: ['*.graphql'],
+      extends: 'plugin:@graphql-eslint/schema-recommended',
+      rules: {
+        '@graphql-eslint/description-style': 0,
+        '@graphql-eslint/strict-id-in-types': [
+          'error',
+          { exceptions: { suffixes: ['Response'] } },
+        ],
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'no-console': 0,
+    'no-underscore-dangle': 0,
+    'no-else-return': ['error', { allowElseIf: true }],
+  },
+};
